5f08d1af2be4286f294a39c57c7325564a1b76c0
cjwalle97 <cjwalle97@gmail.com>
2016-10-27 17:53:45 -0500

    Add files via upload

---
+++ VectorClass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Vector
+{
+private:
+	float m_x;
+	float m_y;
+public:
+	Vector();
+	Vector(float x, float y) {
+		m_x = x;
+		m_y = y;
+	}
+	Vector Add(Vector a, Vector b)
+	{
+		
+	}
+};
+
+int main()
+{
+
+}
